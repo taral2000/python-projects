31d8835adb264ef1d3fcb9a0889f44e5a959b721
unknown <taral2000@gmail.com>
2020-04-12 17:24:25 -0400

    words.py file changes

---
+++ 01 python core/words.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+list = ["test","Oranges","Apple"]
+for i in list:
+    print(i)
+
+def modify(k):
+    k.append("King")
+    print("k = ", k) 
+
+modify(list)
+print("list = ", list)
+
+
+#list list = [1,2,3], slicing
+#del list[index]
+#list.remove("itemname")
+#list.insert(index,item)
+#list.extend(anotherlist)
+#Tuple t = (1,'test', 3.23)
+#range range(stop), range(start, stop), range(start, stop, step)
